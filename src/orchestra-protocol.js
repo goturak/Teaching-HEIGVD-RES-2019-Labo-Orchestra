00538e8f474126ad8e93af6c325f0cbeb4038f30
Goturak <luca-manu.reisdecarvalho@gmail.com>
2019-05-13 10:19:21 +0200

    oops didintpush

---
+++ src/orchestra-protocol.js
@@ -0,0 +1,5 @@
+// Orchestra protocol: send music to multicast group
+// This file specifies the multicast address and the port
+
+exports.PROTOCOL_MULTICAST_ADDRESS = '239.255.22.5';
+exports.PROTOCOL_PORT = 2205;
